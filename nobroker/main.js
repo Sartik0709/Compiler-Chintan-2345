6cd1ff836dd2b1fc9be133e50d37a77edf2df13a
Ashwin <asbhagat2020@gmail.com>
2024-03-29 22:32:31 +0530

    create api

--- nobroker/main.js
+++ nobroker/main.js
@@ -1,9 +1,51 @@
-let hr=document.createElement("hr")
-let navy_con=document.querySelector(".navy")
-let h2=document.createElement("h2");
-h2.innerText="NoBroker services";
-navy_con.append(hr,h2);
-h2.classList.add("high");
+// let URL = "http://localhost:3000/card_Data";
+
+let container = document.getElementById("card-container");
 
 
+async function fetchData(url) {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data);
 
+    } catch (error) {
+        console.log(error);
+    }
+}
+fetchData(`http://localhost:3000/card_Data`);
+
+// function createCard(item){
+//    let card = document.createElement("div");
+//    card.className = "card";
+
+//    let image = document.createElement("img")
+//    image.src = item.image
+//    image.className= card-image;
+   
+//    let name = document.createElement("h3");
+//    name.innerText= item.name;
+   
+//    let rating = document.createElement("p");
+//    rating.innerText = `Rating:${item.rating}`;
+
+//    let review = document.createElement("h4")
+//    review.innerText = item.review;
+
+//    let description = document.createElement("p")
+//    description.innerText = item.description;
+
+//    card.append(image, name, rating, review, description);
+
+//    return card;
+
+// }
+
+// function appendData(data) {
+//     container.innerHTML = "";
+  
+//     data.forEach((item) => {
+//       let card = createCard(item);
+//       container.append(card);
+//     });
+//   }
